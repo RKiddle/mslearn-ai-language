006d6ea45877ee69f75e7133d70613524efaafa7
RKiddle <asiatutors@gmail.com>
2024-10-12 16:32:10 +0700

    nat lang lab 5 done

--- Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/Program.cs
+++ Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/Program.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 
 // import namespaces
-
+ // import namespaces
+ using Azure;
+ using Azure.AI.TextAnalytics;
 
 
 namespace custom_entities
@@ -25,6 +27,11 @@ namespace custom_entities
                 string deploymentName = configuration["Deployment"];
 
                 // Create client using endpoint and key
+                // Create client using endpoint and key
+                AzureKeyCredential credentials = new(aiSvcKey);
+                Uri endpoint = new(aiSvcEndpoint);
+                TextAnalyticsClient aiClient = new(endpoint, credentials);
+
 
 
                 // Read each text file in the ads folder
@@ -46,6 +53,40 @@ namespace custom_entities
                 }
 
                 // Extract entities
+                // Extract entities
+                RecognizeCustomEntitiesOperation operation = await aiClient.RecognizeCustomEntitiesAsync(WaitUntil.Completed, batchedDocuments, projectName, deploymentName);
+
+                await foreach (RecognizeCustomEntitiesResultCollection documentsInPage in operation.Value)
+                {
+                    foreach (RecognizeEntitiesResult documentResult in documentsInPage)
+                    {
+                        Console.WriteLine($"Result for \"{documentResult.Id}\":");
+
+                        if (documentResult.HasError)
+                        {
+                            Console.WriteLine($"  Error!");
+                            Console.WriteLine($"  Document error code: {documentResult.Error.ErrorCode}");
+                            Console.WriteLine($"  Message: {documentResult.Error.Message}");
+                            Console.WriteLine();
+                            continue;
+                        }
+
+                        Console.WriteLine($"  Recognized {documentResult.Entities.Count} entities:");
+
+                        foreach (CategorizedEntity entity in documentResult.Entities)
+                        {
+                            Console.WriteLine($"  Entity: {entity.Text}");
+                            Console.WriteLine($"  Category: {entity.Category}");
+                            Console.WriteLine($"  Offset: {entity.Offset}");
+                            Console.WriteLine($"  Length: {entity.Length}");
+                            Console.WriteLine($"  ConfidenceScore: {entity.ConfidenceScore}");
+                            Console.WriteLine($"  SubCategory: {entity.SubCategory}");
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
 
 
 
--- Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/obj/Debug/net7.0/custom-entities.AssemblyInfo.cs
+++ Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/obj/Debug/net7.0/custom-entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc918dd961618ec358b10359ea5a23bbf8fe831a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff6e4b5883da5e3d5ebcc2d5c9ce326ce52ae1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
